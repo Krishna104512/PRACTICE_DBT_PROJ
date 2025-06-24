df18983f9616414aebcce98bb8690e630c55f30a
Saikrishna <maradana.saikrishna@capgemini.com>
2025-06-24 19:14:48 +0530

    my_first_model_code_commit

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='table') }}
 
-select C_CUSTKEY,C_NAME from {{source('TPCH_SF1','CUSTOMER')}} limit 100
+select C_CUSTKEY,C_NAME from {{source('TPCH_SF1','CUSTOMER')}} limit 1000;
