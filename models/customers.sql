7eefeaacfcf75f583d5a0211cbc9d8ce5b0268c0
Saikrishna <maradana.saikrishna@capgemini.com>
2025-06-25 12:13:23 +0530

    updates to customers

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,4 @@
 {{ config(materialized='table') }}
 
-select C_CUSTKEY,C_NAME from {{source('TPCH_SF1','CUSTOMER')}} limit 1000;
+select C_CUSTKEY,C_NAME from {{source('TPCH_SF1','CUSTOMER')}} limit 1000
+nxA,MN  DCAK,DM AdAlkn dncamA,ḌL'FDC N DKJL
